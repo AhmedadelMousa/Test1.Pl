6b64e99d484b336a6246a4ad2898850d2a224974
Ahmed Adel <Ahmed Adel@LAPTOP-MKPRPSSQ>
2024-09-02 23:21:12 +0300

    Add project files.

---
+++ Test1Demo.DAL/Data/Configration/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test1Demo.DAL.Models;
+
+namespace Test1Demo.DAL.Data.Configration
+{
+    internal class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;DataBase=C42G02Mvc;Trusted_Connection=True }"); 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
